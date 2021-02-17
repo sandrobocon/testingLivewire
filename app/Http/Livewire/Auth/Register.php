4fbf4f1192b9f7a8b94605158ea43f0b7ee4d8d3
Sandro Boçon <sandro_bocon@yahoo.com.br>
2021-02-17 10:49:42 -0300

    Start working with data (register users form)

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $email = '';
+    public $password = '';
+    public $passwordConfirmation = '';
+
+    public function register()
+    {
+        User::create([
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+
+    }
+
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,22 @@
-<div>
-    Hello Registration!!
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="email">Email</label>
+        <input wire:model="email" type="text" id="email" name="email">
+
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input wire:model="email" type="text" id="password" name="password">
+
+    </div>
+    <div>
+        <label for="passwordConfirmation">PasswordConfirmation</label>
+        <input wire:model="email" type="text" id="passwordConfirmation" name="passwordConfirmation">
+
+    </div>
+
+    <div>
+        <input type="submit" value="Register">
+    </div>
+
+</form>
