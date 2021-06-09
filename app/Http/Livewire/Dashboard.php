46af2b817a86e699ee65fcbc518a2631deede9b8
Sandro Boçon <sandro_bocon@yahoo.com.br>
2021-06-09 09:44:47 -0300

    Advanced Search

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Transaction;
+use Illuminate\Support\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,10 +11,18 @@ class Dashboard extends Component
 {
     use WithPagination;
 
-    public $search = '';
     public $sortField = 'title';
     public $sortDirection = 'asc';
     public $showEditModal = false;
+    public $showFilters = false;
+    public $filters = [
+        'search' => '',
+        'status' => '',
+        'amount-min' => null,
+        'amount-max' => null,
+        'date-min' => null,
+        'date-max' => null,
+    ];
     public Transaction $editing;
 
     protected $queryString = ['sortField', 'sortDirection'];
@@ -33,6 +42,11 @@ class Dashboard extends Component
         $this->editing = $this->makeBlankTransaction();
     }
 
+    public function updatedFilters() // Important refresh page!!
+    {
+        $this->resetPage();
+    }
+
     public function sortBy($field)
     {
         if ($this->sortField === $field) {
@@ -47,16 +61,6 @@ class Dashboard extends Component
     public function makeBlankTransaction()
     {
         return Transaction::make(['date' => now(), 'status' => 'success']);
-    }
-
-    public function render()
-    {
-        return view('livewire.dashboard', [
-            'transactions' => Transaction::search('title', $this->search)
-                ->orderBy($this->sortField, $this->sortDirection)
-                ->paginate(10),
-        ])
-            ->layoutData(['header' => 'Dashboard']);
     }
 
     public function create()
@@ -83,5 +87,26 @@ class Dashboard extends Component
         $this->editing->save();
 
         $this->showEditModal = false;
+    }
+
+    public function resetFilters()
+    {
+        $this->reset('filters');
+    }
+
+    public function render()
+    {
+        return view('livewire.dashboard', [
+            'transactions' => Transaction::query()
+                ->when($this->filters['status'], fn($query, $status) => $query->where('status', $status))
+                ->when($this->filters['amount-min'], fn($query, $amount) => $query->where('amount', '>=',  $amount))
+                ->when($this->filters['amount-max'], fn($query, $amount) => $query->where('amount', '<=', $amount))
+                ->when($this->filters['date-min'], fn($query, $date) => $query->where('date', '>=',  Carbon::parse($date)))
+                ->when($this->filters['date-max'], fn($query, $date) => $query->where('date', '<=', Carbon::parse($date)))
+                ->when($this->filters['search'], fn($query, $search) => $query->where('title', 'like', '%' . $search . '%'))
+                ->orderBy($this->sortField, $this->sortDirection)
+                ->paginate(10)
+        ])
+            ->layoutData(['header' => 'Dashboard']);
     }
 }
